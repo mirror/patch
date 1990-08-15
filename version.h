c49261af5aab9064fc90eaabd9c11e96721ec8e2
David J. MacKenzie <djm@wookumz.gnu.ai.mit.edu>
1990-08-15 07:13:39 +0000

    Initial revision

---
+++ version.h
@@ -0,0 +1,15 @@
+/* $Header: /home/agruen/git/patch-h/cvsroot/patch/version.h,v 1.1 1990/08/15 05:13:40 djm Exp $
+ *
+ * $Log: version.h,v $
+ * Revision 1.1  1990/08/15 05:13:40  djm
+ * Initial revision
+ *
+ * Revision 1.1  1990/08/15 05:13:40  djm
+ * Initial revision
+ *
+ * Revision 2.0  86/09/17  15:40:14  lwall
+ * Baseline for netwide release.
+ * 
+ */
+
+void version();
