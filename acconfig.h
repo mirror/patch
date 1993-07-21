59c86f846419b7df215f1887e5dd6a093ab36ba7
David J. MacKenzie <djm@wookumz.gnu.ai.mit.edu>
1993-07-21 02:47:08 +0000

    entered into RCS

---
+++ acconfig.h
@@ -0,0 +1,8 @@
+/* Local acconfig.h for autoheader.
+   Descriptive text for the C preprocessor macros that
+   the patch configure.in can define.
+   autoheader copies the comments into config.h.in.  */
+
+/* Define if you don't have a directory library header.
+   This means you can't make numbered backup files.  */
+#undef NODIR
