2f4f67c5a04e580cb109330563fb5d73b3d69e86
Paul Eggert <eggert@cs.ucla.edu>
1993-07-29 22:11:38 +0000

    Don't exit.

--- version.c
+++ version.c
@@ -1,11 +1,11 @@
-/* $Header: /home/agruen/git/patch-h/cvsroot/patch/version.c,v 1.2 1993/06/11 04:25:56 eggert Exp $
+/* $Header: /home/agruen/git/patch-h/cvsroot/patch/version.c,v 1.3 1993/07/29 20:11:38 eggert Exp $
  *
  * $Log: version.c,v $
- * Revision 1.2  1993/06/11 04:25:56  eggert
- * entered into RCS
+ * Revision 1.3  1993/07/29 20:11:38  eggert
+ * Don't exit.
  *
- * Revision 1.2  1993/06/11 04:25:56  eggert
- * entered into RCS
+ * Revision 1.3  1993/07/29 20:11:38  eggert
+ * Don't exit.
  *
  * Revision 2.0  86/09/17  15:40:11  lwall
  * Baseline for netwide release.
@@ -14,18 +14,14 @@
 
 #include "EXTERN.h"
 #include "common.h"
-#include "util.h"
 #include "INTERN.h"
 #include "patchlevel.h"
 #include "version.h"
 
-void my_exit();
-
-/* Print out the version number and die. */
+/* Print the version number.  */
 
 void
 version()
 {
     fprintf(stderr, "Patch version %s\n", PATCH_VERSION);
-    my_exit(0);
 }
