ca206ada83e4725dbf9432db8c4c830c0e3896d7
Paul Eggert <eggert@cs.ucla.edu>
1993-07-29 22:11:38 +0000

    (pfp, grow_hunkmax, intuit_diff_type, next_intuit_at, skip_to, pfetch, pgetline): No longer exported. (pch_write_line): New declaration. (getline): Removed. n

--- pch.h
+++ pch.h
@@ -1,11 +1,19 @@
-/* $Header: /home/agruen/git/patch-h/cvsroot/patch/pch.h,v 1.3 1993/07/22 19:21:29 djm Exp $
+/* $Header: /home/agruen/git/patch-h/cvsroot/patch/pch.h,v 1.4 1993/07/29 20:11:38 eggert Exp $
  *
  * $Log: pch.h,v $
- * Revision 1.3  1993/07/22 19:21:29  djm
- * entered into RCS
+ * Revision 1.4  1993/07/29 20:11:38  eggert
+ * (pfp, grow_hunkmax, intuit_diff_type, next_intuit_at, skip_to,
+ * pfetch, pgetline): No longer exported.
+ * (pch_write_line): New declaration.
+ * (getline): Removed.
+ * n
  *
- * Revision 1.3  1993/07/22 19:21:29  djm
- * entered into RCS
+ * Revision 1.4  1993/07/29 20:11:38  eggert
+ * (pfp, grow_hunkmax, intuit_diff_type, next_intuit_at, skip_to,
+ * pfetch, pgetline): No longer exported.
+ * (pch_write_line): New declaration.
+ * (getline): Removed.
+ * n
  *
  * Revision 2.0.1.1  87/01/30  22:47:16  lwall
  * Added do_ed_script().
@@ -15,20 +23,15 @@
  * 
  */
 
-EXT FILE *pfp INIT(Nullfp);		/* patch file pointer */
-
 void re_patch PARAMS((void));
 void open_patch_file PARAMS((char *));
 void set_hunkmax PARAMS((void));
-void grow_hunkmax PARAMS((void));
 bool there_is_another_patch PARAMS((void));
-int intuit_diff_type PARAMS((void));
-void next_intuit_at PARAMS((long, long));
-void skip_to PARAMS((long, long));
 bool another_hunk PARAMS((void));
 bool pch_swap PARAMS((void));
 char *pfetch PARAMS((LINENUM));
-short pch_line_len PARAMS((LINENUM));
+void pch_write_line PARAMS((LINENUM, FILE *));
+size_t pch_line_len PARAMS((LINENUM));
 LINENUM pch_first PARAMS((void));
 LINENUM pch_ptrn_lines PARAMS((void));
 LINENUM pch_newfirst PARAMS((void));
@@ -37,7 +40,4 @@ LINENUM pch_end PARAMS((void));
 LINENUM pch_context PARAMS((void));
 LINENUM pch_hunk_beg PARAMS((void));
 char pch_char PARAMS((LINENUM));
-char *pfetch PARAMS((LINENUM));
-int getline PARAMS((char **, int *, FILE *));
-int pgetline PARAMS((char **, int *, FILE *));
 void do_ed_script PARAMS((void));
