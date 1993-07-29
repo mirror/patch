777162e03e219c546c5a9192525619877cadf838
Paul Eggert <eggert@cs.ucla.edu>
1993-07-29 22:11:38 +0000

    (rev_in_string): Remove. (ifetch): Yield size of line too, since strlen no longer applies. (plan_a, plan_b): No longer exported.

--- inp.h
+++ inp.h
@@ -1,24 +1,25 @@
-/* $Header: /home/agruen/git/patch-h/cvsroot/patch/inp.h,v 1.2 1993/07/22 19:17:29 djm Exp $
+/* $Header: /home/agruen/git/patch-h/cvsroot/patch/inp.h,v 1.3 1993/07/29 20:11:38 eggert Exp $
  *
  * $Log: inp.h,v $
- * Revision 1.2  1993/07/22 19:17:29  djm
- * entered into RCS
+ * Revision 1.3  1993/07/29 20:11:38  eggert
+ * (rev_in_string): Remove.
+ * (ifetch): Yield size of line too, since strlen no longer applies.
+ * (plan_a, plan_b): No longer exported.
  *
- * Revision 1.2  1993/07/22 19:17:29  djm
- * entered into RCS
+ * Revision 1.3  1993/07/29 20:11:38  eggert
+ * (rev_in_string): Remove.
+ * (ifetch): Yield size of line too, since strlen no longer applies.
+ * (plan_a, plan_b): No longer exported.
  *
  * Revision 2.0  86/09/17  15:37:25  lwall
  * Baseline for netwide release.
  * 
  */
 
-EXT LINENUM input_lines INIT(0);	/* how long is input file in lines */
-EXT LINENUM last_frozen_line INIT(0);	/* how many input lines have been */
+EXT LINENUM input_lines;		/* how long is input file in lines */
+EXT LINENUM last_frozen_line;		/* how many input lines have been */
 					/* irretractibly output */
 
-bool rev_in_string PARAMS((char *));
+char const *ifetch PARAMS((LINENUM, int, size_t *));
+void re_input PARAMS((void));
 void scan_input PARAMS((char *));
-bool plan_a PARAMS((char *));	/* returns false if insufficient memory */
-void plan_b PARAMS((char *));
-char *ifetch PARAMS((LINENUM, int));
-
