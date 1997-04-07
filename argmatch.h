5ebc3831c3a1e8096e20146669380ca31718290b
Paul Eggert <eggert@cs.ucla.edu>
1997-04-07 07:58:00 +0000

    (__ARGMATCH_P): Renamed from __P, for BSD/OS 3.0.

--- argmatch.h
+++ argmatch.h
@@ -1,14 +1,12 @@
 /* argmatch.h -- declarations for matching arguments against option lists */
 
-#ifndef __P
-# if defined __STDC__ || __GNUC__
-#  define __P(args) args
-# else
-#  define __P(args) ()
-# endif
+#if defined __STDC__ || __GNUC__
+# define __ARGMATCH_P(args) args
+#else
+# define __ARGMATCH_P(args) ()
 #endif
 
-int argmatch __P ((const char *, const char * const *));
-void invalid_arg __P ((const char *, const char *, int));
+int argmatch __ARGMATCH_P ((const char *, const char * const *));
+void invalid_arg __ARGMATCH_P ((const char *, const char *, int));
 
 extern char const program_name[];
