d8c713c737b9e63b1397cea12655c31c5e221beb
Paul Eggert <eggert@cs.ucla.edu>
1997-04-07 03:07:00 +0000

    (pch_prefix_context, pch_suffix_context): New fns replacing pch_context. (another_hunk): Now yields int, not bool; -1 means out of memory. Now takes difftype as argument. (pch_write_line): Now returns boolean indicating whether we're after a newline just after the write, for supporting non-text files.

--- pch.h
+++ pch.h
@@ -1,43 +1,23 @@
-/* $Header: /home/agruen/git/patch-h/cvsroot/patch/pch.h,v 1.4 1993/07/29 20:11:38 eggert Exp $
- *
- * $Log: pch.h,v $
- * Revision 1.4  1993/07/29 20:11:38  eggert
- * (pfp, grow_hunkmax, intuit_diff_type, next_intuit_at, skip_to,
- * pfetch, pgetline): No longer exported.
- * (pch_write_line): New declaration.
- * (getline): Removed.
- * n
- *
- * Revision 1.4  1993/07/29 20:11:38  eggert
- * (pfp, grow_hunkmax, intuit_diff_type, next_intuit_at, skip_to,
- * pfetch, pgetline): No longer exported.
- * (pch_write_line): New declaration.
- * (getline): Removed.
- * n
- *
- * Revision 2.0.1.1  87/01/30  22:47:16  lwall
- * Added do_ed_script().
- * 
- * Revision 2.0  86/09/17  15:39:57  lwall
- * Baseline for netwide release.
- * 
- */
+/* reading patches */
 
-void re_patch PARAMS((void));
-void open_patch_file PARAMS((char *));
-void set_hunkmax PARAMS((void));
-bool there_is_another_patch PARAMS((void));
-bool another_hunk PARAMS((void));
-bool pch_swap PARAMS((void));
-char *pfetch PARAMS((LINENUM));
-void pch_write_line PARAMS((LINENUM, FILE *));
-size_t pch_line_len PARAMS((LINENUM));
-LINENUM pch_first PARAMS((void));
-LINENUM pch_ptrn_lines PARAMS((void));
-LINENUM pch_newfirst PARAMS((void));
-LINENUM pch_repl_lines PARAMS((void));
-LINENUM pch_end PARAMS((void));
-LINENUM pch_context PARAMS((void));
-LINENUM pch_hunk_beg PARAMS((void));
-char pch_char PARAMS((LINENUM));
-void do_ed_script PARAMS((void));
+/* $Id: pch.h,v 1.5 1997/04/07 01:07:00 eggert Exp $ */
+
+LINENUM pch_end PARAMS ((void));
+LINENUM pch_first PARAMS ((void));
+LINENUM pch_hunk_beg PARAMS ((void));
+LINENUM pch_newfirst PARAMS ((void));
+LINENUM pch_prefix_context PARAMS ((void));
+LINENUM pch_ptrn_lines PARAMS ((void));
+LINENUM pch_repl_lines PARAMS ((void));
+LINENUM pch_suffix_context PARAMS ((void));
+bool pch_swap PARAMS ((void));
+bool pch_write_line PARAMS ((LINENUM, FILE *));
+bool there_is_another_patch PARAMS ((void));
+char *pfetch PARAMS ((LINENUM));
+char pch_char PARAMS ((LINENUM));
+int another_hunk PARAMS ((enum diff));
+size_t pch_line_len PARAMS ((LINENUM));
+void do_ed_script PARAMS ((FILE *));
+void open_patch_file PARAMS ((char const *));
+void re_patch PARAMS ((void));
+void set_hunkmax PARAMS ((void));
