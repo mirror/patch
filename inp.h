e57b422639c981987eca6536b14cff0f3f312476
Paul Eggert <eggert@cs.ucla.edu>
1997-04-07 03:07:00 +0000

    (get_input_file): New decl.

--- inp.h
+++ inp.h
@@ -1,25 +1,10 @@
-/* $Header: /home/agruen/git/patch-h/cvsroot/patch/inp.h,v 1.3 1993/07/29 20:11:38 eggert Exp $
- *
- * $Log: inp.h,v $
- * Revision 1.3  1993/07/29 20:11:38  eggert
- * (rev_in_string): Remove.
- * (ifetch): Yield size of line too, since strlen no longer applies.
- * (plan_a, plan_b): No longer exported.
- *
- * Revision 1.3  1993/07/29 20:11:38  eggert
- * (rev_in_string): Remove.
- * (ifetch): Yield size of line too, since strlen no longer applies.
- * (plan_a, plan_b): No longer exported.
- *
- * Revision 2.0  86/09/17  15:37:25  lwall
- * Baseline for netwide release.
- * 
- */
+/* inputting files to be patched */
 
-EXT LINENUM input_lines;		/* how long is input file in lines */
-EXT LINENUM last_frozen_line;		/* how many input lines have been */
-					/* irretractibly output */
+/* $Id: inp.h,v 1.4 1997/04/07 01:07:00 eggert Exp $ */
 
-char const *ifetch PARAMS((LINENUM, int, size_t *));
-void re_input PARAMS((void));
-void scan_input PARAMS((char *));
+XTERN LINENUM input_lines;		/* how long is input file in lines */
+
+char const *ifetch PARAMS ((LINENUM, int, size_t *));
+void get_input_file PARAMS ((char const *, char const *));
+void re_input PARAMS ((void));
+void scan_input PARAMS ((char *));
