fc4b8086c3bddb4a56cee2085f2a480c5d839183
Paul Eggert <eggert@cs.ucla.edu>
1997-04-07 03:07:00 +0000

    (NODIR): Remove. (HAVE_MEMCHR): Add.

--- acconfig.h
+++ acconfig.h
@@ -1,8 +1,7 @@
 /* Local acconfig.h for autoheader.
    Descriptive text for the C preprocessor macros that
    the patch configure.in can define.
-   autoheader copies the comments into config.h.in.  */
+   autoheader copies the comments into config.hin.  */
 
-/* Define if you don't have a directory library header.
-   This means you can't make numbered backup files.  */
-#undef NODIR
+/* Define if memchr works.  */
+#undef HAVE_MEMCHR
