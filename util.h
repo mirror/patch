9f028e37d575476637b20bfdfa66748a9dd4dda3
Paul Eggert <eggert@cs.ucla.edu>
1997-04-07 03:07:00 +0000

    (say1, say2, say3, say4, fatal1, fatal2, fatal3, fatal4, pfatal1, pfatal2, pfatal3, pfatal4, ask1, ask2, ask3, ask4): Remove; replaced with following. (ask, say, fatal, pfatal): New stdarg functions. (fetchname): Remove last, `assume_exists' parameter. (savebuf, savestr, move_file, copy_file): Args are now const *. (exit_with_signal): New function, for proper process status if a signal is received as per POSIX.2. (basenamse): Rename to `base_name' and move to backupfile.

--- util.h
+++ util.h
@@ -1,49 +1,26 @@
-/* $Header: /home/agruen/git/patch-h/cvsroot/patch/util.h,v 1.5 1993/07/30 02:02:51 eggert Exp $
- *
- * $Log: util.h,v $
- * Revision 1.5  1993/07/30 02:02:51  eggert
- * (makedirs): Omit `striplast' argument.  Remove `aask'.
- *
- * Revision 1.5  1993/07/30 02:02:51  eggert
- * (makedirs): Omit `striplast' argument.  Remove `aask'.
- *
- * Revision 2.0  86/09/17  15:40:06  lwall
- * Baseline for netwide release.
- * 
- */
+/* utility functions for `patch' */
 
-#define say1(a) (fprintf (stderr, a), Fflush (stderr))
-#define say2(a,b) (fprintf (stderr, a,b), Fflush (stderr))
-#define say3(a,b,c) (fprintf (stderr, a,b,c), Fflush (stderr))
-#define say4(a,b,c,d) (fprintf (stderr, a,b,c,d), Fflush (stderr))
-#define fatal1(a) (fprintf (afatal (), a), zfatal ())
-#define fatal2(a,b) (fprintf (afatal (), a,b), zfatal ())
-#define fatal3(a,b,c) (fprintf (afatal (), a,b,c), zfatal ())
-#define fatal4(a,b,c,d) (fprintf (afatal (), a,b,c,d), zfatal ())
-#define pfatal1(a) (fprintf (apfatal (), a), zpfatal ())
-#define pfatal2(a,b) (fprintf (apfatal (), a,b), zpfatal ())
-#define pfatal3(a,b,c) (fprintf (apfatal (), a,b,c), zpfatal ())
-#define pfatal4(a,b,c,d) (fprintf (apfatal (), a,b,c,d), zpfatal ())
-#define ask1(a) (Sprintf (buf, a), zask())
-#define ask2(a,b) (Sprintf (buf, a,b), zask())
-#define ask3(a,b,c) (Sprintf (buf, a,b,c), zask())
-#define ask4(a,b,c,d) (Sprintf (buf, a,b,c,d), zask())
+/* $Id: util.h,v 1.6 1997/04/07 01:07:00 eggert Exp $ */
 
-char *fetchname PARAMS((char *, int, int));
-int move_file PARAMS((char *, char *));
-void copy_file PARAMS((char *, char *));
-char *savebuf PARAMS((char *, size_t));
-char *savestr PARAMS((char *));
-FILE *afatal PARAMS((void));
-EXITING void zfatal PARAMS((void));
-void memory_fatal PARAMS((void));
-void read_fatal PARAMS((void));
-void write_fatal PARAMS((void));
-FILE *apfatal PARAMS((void));
-EXITING void zpfatal PARAMS((void));
-void zask PARAMS((void));
-void set_signals PARAMS((int));
-void ignore_signals PARAMS((void));
-void makedirs PARAMS((char *));
-char *basename PARAMS((char *));
-void Fseek PARAMS((FILE *, long, int));
+void ask PARAMS ((char const *, ...)) __attribute__ ((format (printf, 1, 2)));
+void say PARAMS ((char const *, ...)) __attribute__ ((format (printf, 1, 2)));
+
+void fatal PARAMS ((char const *, ...))
+	__attribute__ ((noreturn, format (printf, 1, 2)));
+void pfatal PARAMS ((char const *, ...))
+	__attribute__ ((noreturn, format (printf, 1, 2)));
+
+char *fetchname PARAMS ((char *, int));
+char *savebuf PARAMS ((char const *, size_t));
+char *savestr PARAMS ((char const *));
+void Fseek PARAMS ((FILE *, long, int));
+void copy_file PARAMS ((char const *, char const *));
+void exit_with_signal PARAMS ((int)) __attribute__ ((noreturn));
+void ignore_signals PARAMS ((void));
+void makedirs PARAMS ((char *));
+void memory_fatal PARAMS ((void));
+void move_file PARAMS ((char const *, char const *, int));
+void read_fatal PARAMS ((void));
+void remove_prefix PARAMS ((char *, size_t));
+void set_signals PARAMS ((int));
+void write_fatal PARAMS ((void));
