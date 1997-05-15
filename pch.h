b36226e2c9f99d9b08a8222a14acb220fb033d8a
Paul Eggert <eggert@cs.ucla.edu>
1997-05-15 19:59:15 +0000

    (pch_says_nonexistent): New decl.

--- pch.h
+++ pch.h
@@ -1,6 +1,6 @@
 /* reading patches */
 
-/* $Id: pch.h,v 1.5 1997/04/07 01:07:00 eggert Exp $ */
+/* $Id: pch.h,v 1.6 1997/05/15 17:59:15 eggert Exp $ */
 
 LINENUM pch_end PARAMS ((void));
 LINENUM pch_first PARAMS ((void));
@@ -16,6 +16,7 @@ bool there_is_another_patch PARAMS ((void));
 char *pfetch PARAMS ((LINENUM));
 char pch_char PARAMS ((LINENUM));
 int another_hunk PARAMS ((enum diff));
+int pch_says_nonexistent PARAMS ((int));
 size_t pch_line_len PARAMS ((LINENUM));
 void do_ed_script PARAMS ((FILE *));
 void open_patch_file PARAMS ((char const *));
