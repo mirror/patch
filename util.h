dedf7e88c8875835b4c40a50c337f4eeb79d9ab1
Paul Eggert <eggert@cs.ucla.edu>
1997-05-15 19:59:15 +0000

    (replace_slashes): No longer exported. (fetchname): Add support for deleted files. (copy_file, move_file): Add support for preserving file modes. (init_time, removedirs): New functions.

--- util.h
+++ util.h
@@ -1,6 +1,6 @@
 /* utility functions for `patch' */
 
-/* $Id: util.h,v 1.10 1997/05/06 12:30:13 eggert Exp $ */
+/* $Id: util.h,v 1.11 1997/05/15 17:59:15 eggert Exp $ */
 
 void ask PARAMS ((char const *, ...)) __attribute__ ((format (printf, 1, 2)));
 void say PARAMS ((char const *, ...)) __attribute__ ((format (printf, 1, 2)));
@@ -10,19 +10,20 @@ void fatal PARAMS ((char const *, ...))
 void pfatal PARAMS ((char const *, ...))
 	__attribute__ ((noreturn, format (printf, 1, 2)));
 
-char *fetchname PARAMS ((char *, int));
-char *replace_slashes PARAMS ((char *));
+char *fetchname PARAMS ((char *, int, int *));
 char *savebuf PARAMS ((char const *, size_t));
 char *savestr PARAMS ((char const *));
 int systemic PARAMS ((char const *));
 void Fseek PARAMS ((FILE *, file_offset, int));
-void copy_file PARAMS ((char const *, char const *));
+void copy_file PARAMS ((char const *, struct stat const *, char const *));
 void exit_with_signal PARAMS ((int)) __attribute__ ((noreturn));
 void ignore_signals PARAMS ((void));
+void init_time PARAMS ((void));
 void makedirs PARAMS ((char *));
 void memory_fatal PARAMS ((void));
-void move_file PARAMS ((char const *, char const *, int));
+void move_file PARAMS ((char const *, struct stat const *, char const *, int));
 void read_fatal PARAMS ((void));
 void remove_prefix PARAMS ((char *, size_t));
+void removedirs PARAMS ((char *));
 void set_signals PARAMS ((int));
 void write_fatal PARAMS ((void));
