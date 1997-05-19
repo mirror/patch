1fb15ad5f710595806542730aad89c2e016893d2
Paul Eggert <eggert@cs.ucla.edu>
1997-05-19 08:52:03 +0000

    (create_file): New function. (copy_file): Now takes mode, not struct stat. (makedirs): No longer exported. (move_file): Now takes mode, not struct stat.

--- util.h
+++ util.h
@@ -1,7 +1,8 @@
 /* utility functions for `patch' */
 
-/* $Id: util.h,v 1.11 1997/05/15 17:59:15 eggert Exp $ */
+/* $Id: util.h,v 1.12 1997/05/19 06:52:03 eggert Exp $ */
 
+int ok_to_reverse PARAMS ((char const *, ...)) __attribute__ ((format (printf, 1, 2)));
 void ask PARAMS ((char const *, ...)) __attribute__ ((format (printf, 1, 2)));
 void say PARAMS ((char const *, ...)) __attribute__ ((format (printf, 1, 2)));
 
@@ -13,15 +14,15 @@ void pfatal PARAMS ((char const *, ...))
 char *fetchname PARAMS ((char *, int, int *));
 char *savebuf PARAMS ((char const *, size_t));
 char *savestr PARAMS ((char const *));
+int create_file PARAMS ((char const *, int, mode_t));
 int systemic PARAMS ((char const *));
 void Fseek PARAMS ((FILE *, file_offset, int));
-void copy_file PARAMS ((char const *, struct stat const *, char const *));
+void copy_file PARAMS ((char const *, char const *, mode_t));
 void exit_with_signal PARAMS ((int)) __attribute__ ((noreturn));
 void ignore_signals PARAMS ((void));
 void init_time PARAMS ((void));
-void makedirs PARAMS ((char *));
 void memory_fatal PARAMS ((void));
-void move_file PARAMS ((char const *, struct stat const *, char const *, int));
+void move_file PARAMS ((char const *, char *, mode_t, int));
 void read_fatal PARAMS ((void));
 void remove_prefix PARAMS ((char *, size_t));
 void removedirs PARAMS ((char *));
