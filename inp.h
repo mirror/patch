2e4e61c3d0e710ca278378e7ed406218677e3f5f
Paul Eggert <eggert@cs.ucla.edu>
2002-05-28 09:12:03 +0000

    Add copyright notice, remove PARAM.

--- inp.h
+++ inp.h
@@ -1,10 +1,29 @@
 /* inputting files to be patched */
 
-/* $Id: inp.h,v 1.5 1999/10/11 03:33:08 eggert Exp $ */
+/* $Id: inp.h,v 1.6 2002/05/28 07:12:03 eggert Exp $ */
+
+/* Copyright (C) 1986, 1988 Larry Wall
+   Copyright (C) 1991, 1992, 1993, 1997, 1998, 1999, 2002 Free
+   Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.
+   If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 XTERN LINENUM input_lines;		/* how long is input file in lines */
 
-char const *ifetch PARAMS ((LINENUM, int, size_t *));
-void get_input_file PARAMS ((char const *, char const *));
-void re_input PARAMS ((void));
-void scan_input PARAMS ((char *));
+char const *ifetch (LINENUM, int, size_t *);
+void get_input_file (char const *, char const *);
+void re_input (void);
+void scan_input (char *);
