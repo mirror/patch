133f2820b3d3b21fbac1103d9ae0bbde7f0091a2
Andreas Gruenbacher <agruen@suse.de>
2010-04-27 09:54:49 +0200

    Do not redefine __attribute__ when it is already defined
    * src/common.h: Do not redefine __attribute__ when it is already
    defined.

--- src/common.h
+++ src/common.h
@@ -130,8 +130,16 @@ XTERN enum diff diff_type;
 
 XTERN char *revision;			/* prerequisite revision, if any */
 
-#if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 6) || __STRICT_ANSI__
-# define __attribute__(x)
+#ifndef __attribute__
+/* The __attribute__ feature is available in gcc versions 2.5 and later.
+   The __-protected variants of the attributes 'format' and 'printf' are
+   accepted by gcc versions 2.6.4 (effectively 2.7) and later.
+   We enable __attribute__ only if these are supported too, because
+   gnulib and libintl do '#define printf __printf__' when they override
+   the 'printf' function.  */
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
+#  define __attribute__(Spec)   /* empty */
+# endif
 #endif
 
 void fatal_exit (int) __attribute__ ((noreturn));
