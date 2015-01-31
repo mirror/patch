b72e3be5c8a75f310924dae660f3f506112a2062
Andreas Gruenbacher <agruen@gnu.org>
2015-01-31 03:15:28 +0100

    Update list of gnulib modules used
    * bootstrap.conf (gnulib_modules): Remove lchmod, lstat, mkdir, readlink,
    rename, mkdir, symlink, unlink, utimens. Add fchownat, fchmodat, fstatat,
    mkdirat, openat, readlinkat, renameat, symlinkat, unlinkat, utimensat.
    * src/util.h: Don't include <utimens.h> anymore.

--- src/util.h
+++ src/util.h
@@ -18,7 +18,6 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-#include <utimens.h>
 #include <timespec.h>
 #include <stat-time.h>
 #include <backupfile.h>
